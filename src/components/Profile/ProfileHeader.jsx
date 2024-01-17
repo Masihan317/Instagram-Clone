777c71649d5b96c14485f9287bb8ecef6145be7e
sihanm2 <sihanm2@uw.edu>
2024-01-17 00:02:49 -0800

    5:27:30

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -2,11 +2,13 @@ import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from "
 import useUserProfileStore from "../../store/userProfileStore"
 import useAuthStore from "../../store/authStore"
 import EditProfile from "./EditProfile";
+import useFollow from "../../hooks/useFollow";
 
 const ProfileHeader = () => {
   const { userProfile } = useUserProfileStore();
   const authUser = useAuthStore(state => state.user)
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isLoading, isFollowing, handleFollow } = useFollow(userProfile?.uid);
 
   return (
     <Flex gap={{ base: 4, sm: 10 }} py={10} direction={{ base: "column", sm: "row" }}>
@@ -24,8 +26,8 @@ const ProfileHeader = () => {
             </Flex>)}
             {authUser && authUser.username !== userProfile.username && (
             <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-              <Button bg={"blue.500"} color={"white"} _hover={{ bg: "blue.600" }} size={{ base: "xs", md: "sm" }}>
-                Follow
+              <Button bg={"blue.500"} color={"white"} _hover={{ bg: "blue.600" }} size={{ base: "xs", md: "sm" }} onClick={handleFollow} isLoading={isLoading} >
+                { isFollowing ? "Unfollow" : "Follow" }
               </Button>
             </Flex>)}
         </Flex>
---
+++ src/hooks/useFollow.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react"
+import useAuthStore from "../store/authStore";
+import useUserProfileStore from "../store/userProfileStore";
+import useShowToast from "./useShowToast";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
+const useFollow = (uid) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const authUser = useAuthStore(state => state.user);
+  const setAuthUser = useAuthStore(state => state.setUser);
+  const { userProfile, setUserProfile } = useUserProfileStore();
+  const showToast = useShowToast();
+
+  const handleFollow = async () => {
+
+    setIsLoading(true);
+
+    try {
+      const currentUserRef = doc(firestore, "users", authUser.uid);
+      const otherUserRef = doc(firestore, "users", uid);
+      await updateDoc(currentUserRef, {
+        following: isFollowing ? arrayRemove(uid) : arrayUnion(uid)
+      })
+      await updateDoc(otherUserRef, {
+        followers: isFollowing ? arrayRemove(authUser.uid) : arrayUnion(authUser.uid)
+      })
+
+      if (isFollowing) {
+        setAuthUser({
+          ...authUser,
+          following: authUser.following.filter(id => id !== uid)
+        })
+        setUserProfile({
+          ...userProfile,
+          followers: userProfile.followers.filter(id => id !== authUser.uid)
+        })
+
+        localStorage.setItem("user-info", JSON.stringify({
+          ...authUser,
+          following: authUser.following.filter(id => id !== uid)
+        }))
+
+        setIsFollowing(false)
+      } else {
+        setAuthUser({
+          ...authUser,
+          following: [...authUser.following, uid]
+        })
+        setUserProfile({
+          ...userProfile,
+          followers: [...userProfile.followers, authUser.uid]
+        })
+        localStorage.setItem("user-info", JSON.stringify({
+          ...authUser,
+          following: [...authUser.following, uid]
+        }))
+
+        setIsFollowing(true);
+      }
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (authUser) {
+      const isFollowing = authUser.following.includes(uid);
+      setIsFollowing(isFollowing)
+    }
+  }, [authUser, uid])
+
+  return { isLoading, isFollowing, handleFollow }
+}
+
+export default useFollow
